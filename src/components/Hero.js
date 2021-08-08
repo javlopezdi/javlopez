82786438cfbea6c7bb47372e678788d1d0b3a427
Torjer <andres22jld@gmail.com>
2021-08-08 11:09:11 -0500

    Test commit
    Test commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Hero = () => {
-  return <div className="bg-blue-700">Hero</div>;
+  return (
+    <div className="bg-blue-700">
+      <div className="container mx-auto">Container</div>
+    </div>
+  );
 };
 
 export default Hero;
